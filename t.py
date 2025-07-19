2c466688c4db6375f7dc0615552233eb9a1279b7
Sebastian Stefenel <stefenel.sebastian@gmail.com>
2025-07-19 15:54:43 -0400

    remove finhub

--- t.py
+++
@@ -1,6 +0,0 @@
-import finnhub
-from key import API_KEY
-
-finnhub_client = finnhub.Client(api_key=API_KEY)
-
-print(finnhub_client.earnings_calendar(_from="2021-06-10", to="2021-06-30", symbol="AAPL", international=False))
--- tst.py
+++
@@ -1,9 +0,0 @@
-import requests
-from key import API_KEY
-
-response = requests.get("https://finnhub.io/api/v1/quote", params={
-    'symbol': 'AAPL',
-    'token': API_KEY
-})
-
-print(response.json())
