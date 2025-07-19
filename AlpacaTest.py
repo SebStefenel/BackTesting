72e9c94422b344d19e8fd0e3ce2a902efdef2395
Sebastian Stefenel <stefenel.sebastian@gmail.com>
2025-07-19 15:50:57 -0400

    change API keys

---
+++ AlpacaTest.py
@@ -0,0 +1,26 @@
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+import datetime
+from key import API_KEY, Secret
+
+# Replace with your keys
+ALPACA_API_KEY = API_KEY
+ALPACA_SECRET_KEY = Secret
+
+# Create Alpaca data client
+client = StockHistoricalDataClient(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+
+# Request 1-minute bars for AAPL
+request_params = StockBarsRequest(
+    symbol_or_symbols=["AAPL"],
+    timeframe=TimeFrame.Minute,
+    start=datetime.datetime(2024, 7, 1),
+    end=datetime.datetime(2024, 7, 2)
+)
+
+# Fetch data
+bars = client.get_stock_bars(request_params)
+
+# Print the data as a DataFrame
+print(bars.df)
--- key.py
+++ key.py
@@ -1,5 +1,6 @@
 # This gets the API key from a file on my computer, if you wish to use this code, please replace the file path with your own API key
-with open("C:\\Users\\sstefenel\\Desktop\\FinnhubAPIkey.txt", "r", encoding="utf-16") as file:
-    sstefenel_API_KEY = file.read().strip()
+with open("C:\\Users\\sstefenel\\Desktop\\Alpaca.txt", "r", encoding="utf-8") as file:
+    lines = file.read().splitlines()
 
-API_KEY = sstefenel_API_KEY 
+API_KEY = lines[0].split()[0]
+Secret = lines[0].split()[1]
