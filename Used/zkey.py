429bd9992e5a77f9400f17a637a582f68d11fe01
Sebastian Stefenel <stefenel.sebastian@gmail.com>
2025-08-03 22:00:09 -0400

    git

--- Used/zkey.py
+++
@@ -1,6 +0,0 @@
-# This gets the API key from a file on my computer, if you wish to use this code, please replace the file path with your own API key
-with open("C:\\Users\\sstefenel\\Desktop\\Alpaca.txt", "r", encoding="utf-8") as file:
-    lines = file.read().splitlines()
-
-API_KEY = lines[0].split()[0]
-Secret = lines[0].split()[1]
