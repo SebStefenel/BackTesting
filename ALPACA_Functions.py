e9509b7ab5f7f076367d93605f30d47dea06c960
Sebastian Stefenel <stefenel.sebastian@gmail.com>
2025-10-07 00:00:12 -0400

    API_Key change

--- ALPACA_Functions.py
+++ ALPACA_Functions.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # This gets the API key from a file on my computer, if you wish to use this code, please replace the file path with your own API key
 def get_key():
-    with open("C:\\Users\\sstefenel\\Desktop\\Alpaca.txt", "r", encoding="utf-8") as file:
+    with open('API_Key.txt', "r", encoding="utf-8") as file:
         lines = file.read().splitlines()
 
     API_KEY = lines[0].split()[0]
